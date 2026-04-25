67a476d7224d45c1a6caa1bc6d30246d381720b1
Eldeston <fremfredric@gmail.com>
2026-04-25 22:57:07 +0400

    Removed obsolete imports

--- app/src/main/java/com/example/temperamentcalculator/AboutActivity.kt
+++ app/src/main/java/com/example/temperamentcalculator/AboutActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.temperamentcalculator.databinding.ActivityAboutBinding
-import com.example.temperamentcalculator.databinding.ActivityMainBinding
 
 class AboutActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAboutBinding
--- app/src/main/java/com/example/temperamentcalculator/ResultsActivity.kt
+++ app/src/main/java/com/example/temperamentcalculator/ResultsActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.temperamentcalculator.databinding.ActivityMainBinding
 import com.example.temperamentcalculator.databinding.ActivityResultsBinding
 
 class ResultsActivity : AppCompatActivity() {
